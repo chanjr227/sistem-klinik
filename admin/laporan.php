37189722e5cf6449fc05162fa92fd2ecfde83f67
chanjr227 <chandrajr227@gmail.com>
2026-04-29 16:21:37 +0700

    sidebar laporan

--- admin/laporan.php
+++ admin/laporan.php
@@ -24,6 +24,7 @@ $result = $koneksi->query($sql);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
     <!-- CSS -->
+    <link rel="stylesheet" href="assets/dashboard.css">
     <link rel="stylesheet" href="assets/laporan.css">
 </head>
 
